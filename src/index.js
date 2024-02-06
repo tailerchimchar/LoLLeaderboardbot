7b0a81d38293c2b9cec2383a00f0b7c51c1f9c75
Tailer Nguyen <35902625+tailerchimchar@users.noreply.github.com>
2024-02-05 19:00:40 -0600

    adding in mongoose

--- src/index.js
+++ src/index.js
@@ -1,26 +1,120 @@
 require('dotenv').config();
 const {Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, SlashCommandBuilder} = require('discord.js');
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]})
+const mongoose = require('mongoose');
+
+// creating summoner schema
+const summonerSchema = new mongoose.Schema({
+  IGN: { type: String, required: true },
+  Rank: { type: String, required: true },
+  LP: { type: Number, required: true },
+  Wins: { type: Number, required: true },
+  Losses: { type: Number, required: true },
+  WinRate: { type: Number, required: true }
+});
+
+const Summoner = mongoose.model('Summoner', summonerSchema);
 
 client.on("ready", (x) => {
-   // Making sure that the author of the message is not a bot.
-  if (message.author.bot) return false;
-
   console.log(`${x.user.tag} is ready! `);
-  client.user.setActivity('added in addsummoner');
+  client.user.setActivity('adding in fetchallsummoners');
 
-  const ping = new SlashCommandBuilder()
+  // creating commands
+  const getallsummoners = new SlashCommandBuilder()
+  .setName('getallsummoners')
+  .setDescription("Retrieve all summoners from the database");
+
+  const addsummoner = new SlashCommandBuilder()
   .setName('addsummoner')
   .setDescription("Adding in a summoner to the database");
 
-  client.application.commands.create(ping);
+  connectToDatabase();
+
+  // adding commands to bot
+  try{
+    client.application.commands.create(getallsummoners);
+    client.application.commands.create(addsummoner);
+
+    console.log('Commands registered successfully');
+
+  }catch( error ){
+    console.error('Error registering commands:', error);
+  }
 })
 
-client.on('interactionCreate', (interaction) =>{
+client.on('interactionCreate', async (interaction) =>{
   if(!interaction.isChatInputCommand()) return;
+  
   if(interaction.commandName==='addsummoner'){
     interaction.reply('adding in a new summoner!');
   }
-})
+  else if (interaction.commandName === 'getallsummoners') 
+  {
+    try 
+    {
+      const summoners = await Summoner.find();
+      let message = 'Summoners:\n';
+      summoners.forEach((summoner, index) => {
+        message += `${index + 1}. IGN: ${summoner.IGN}, Rank: ${summoner.Rank}, LP: ${summoner.LP}, Wins: ${summoner.Wins}, Losses: ${summoner.Losses}, WinRate: ${summoner.WinRate}%\n`;
+      });
+      await interaction.reply(message);
+    } catch (error) {
+      console.error('Error fetching summoners:', error);
+      await interaction.reply('Failed to fetch summoners.');
+    }
+  }
+});
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to db");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function addSummoner(IGN, Rank, LP, Wins, Losses) {
+  const winRate = Wins / (Wins + Losses) * 100;
+  
+  const summoner = new Summoner({
+    IGN,
+    Rank,
+    LP,
+    Wins,
+    Losses,
+    WinRate: winRate
+  });
+
+  try {
+    const result = await summoner.save();
+    console.log(result);
+  } catch (error) {
+    console.error('Error saving the summoner:', error);
+  }
+}
+
+//addSummoner('Summoner1', 'Grandmaster', 500, 200, 100);
+//addSummoner('Summoner2', 'Master', 400, 150, 80);
+//addSummoner('Summoner3', 'Diamond', 300, 120, 60);
+//addSummoner('Summoner4', 'Platinum', 200, 100, 50);
+//addSummoner('Summoner5', 'Gold', 100, 80, 40);
+
+//addSummoner('issariu', 'Grandmaster', 382, 79, 55)
+
+async function fetchAllSummoners() {
+  try {
+    const summoners = await Summoner.find(); 
+    console.log(summoners);
+  } catch (error) {
+    console.error('Error fetching summoners:', error);
+  }
+}
+
+fetchAllSummoners();
+
 
 client.login(process.env.TOKEN);
